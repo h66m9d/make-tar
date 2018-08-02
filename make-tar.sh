3b3e2aa02d74cb56f870b46724355b925f62ef29
h66m9d <h66m9d@users.noreply.github.com>
2018-08-02 19:05:09 +0430

    Update make-tar.sh

--- make-tar.sh
+++ make-tar.sh
@@ -1,4 +1,5 @@
-read -p "Path name" -e -i 2 todo
+read -p "Path name" -e -i 2 archivename
 		archivenamenonespace=`echo $archivename | tr ' ' '_'`
+		mv "$archivename" $archivenamenonespace
 		tar -czvf $archivenamenonespace.tar.gz $archivenamenonespace/* --remove-files
 		rm -rf $archivenamenonespace
